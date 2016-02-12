ac2c7a55a4f37ed67d84ec056ab9e64ebee5e8c0
chris hogan <chogan@hogan.io>
2016-02-12 00:08:53 -0700

    BOP-317 model for Units form created

--- packages/custom/unit/server/routes/unit.js
+++ packages/custom/unit/server/routes/unit.js
@@ -1,29 +1,37 @@
-(function () {
-  'use strict';
+'use strict';
 
-  /* jshint -W098 */
-  // The Package is past automatically as first parameter
-  module.exports = function (Unit, app, auth, database) {
+// unit authorization helpers
+var hasAuthorization = function(req, res, next) {
+  if (!req.user.isAdmin && !req.user.user._id.equals(req.user._id)) {
+    return res.status(401).send('User is not authorized');
+  }
+  next();
+};
 
-    app.get('/api/unit/example/anyone', function (req, res, next) {
-      res.send('Anyone can access this');
-    });
+var hasPermissions = function(req, res, next) {
 
-    app.get('/api/unit/example/auth', auth.requiresLogin, function (req, res, next) {
-      res.send('Only authenticated users can access this');
-    });
+    req.body.permissions = req.body.permissions || ['authenticated'];
 
-    app.get('/api/unit/example/admin', auth.requiresAdmin, function (req, res, next) {
-      res.send('Only users with Admin role can access this');
-    });
+    for (var i = 0; i < req.body.permissions.length; i++) {
+      var permission = req.body.permissions[i];
+      if (req.acl.user.allowed.indexOf(permission) === -1) {
+            return res.status(401).send('User not allowed to assign ' + permission + ' permission.');
+        }
+    }
 
-    app.get('/api/unit/example/render', function (req, res, next) {
-      Unit.render('index', {
-        package: 'unit'
-      }, function (err, html) {
-        //Rendering a view from the Package server/views
-        res.send(html);
-      });
-    });
-  };
-})();
+    next();
+};
+
+module.exports = function(Unit, app, auth) {
+
+  var unit = require('../controllers/unit')(Unit);
+
+  app.route('/api/units')
+    .get(unit.all)
+    .post(auth.requiresLogin, hasPermissions, unit.create);
+  app.route('/api/units/:unitId')
+    .get(auth.isMongoId, unit.unit);
+
+  // Finish with setting up the unitId param
+  app.param('unitId', unit.unit);
+};
