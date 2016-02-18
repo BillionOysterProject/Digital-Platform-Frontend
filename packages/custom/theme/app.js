3bd1bb02581c9faf3372f8e9e6f0dc75c9f871ee
Felix Gilbert <felixgilbert@Felixs-Mac-Pro.fios-router.home>
2016-02-18 12:56:48 -0500

    Added styles to BOP-417

--- packages/custom/theme/app.js
+++ packages/custom/theme/app.js
@@ -25,7 +25,8 @@ Theme.register(function(app, auth, database) {
 //  });
 
   Theme.aggregateAsset('css', 'loginForms.css');
-  Theme.aggregateAsset('css', 'theme.css');
+//  Theme.aggregateAsset('css', 'theme.css');
+  Theme.aggregateAsset('css', 'bop-main.css', {global:true});
   Theme.aggregateAsset('css', 'bop-style.css', {global:true});
   Theme.angularDependencies(['mean.system']);
 
