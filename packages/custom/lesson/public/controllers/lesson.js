57bc88f53d6189f2832315d89250172a551679e9
chris hogan <chogan@hogan.io>
2016-02-27 00:59:28 -0500

    chore(textInput, dropdown, refactoring): Made sure the direcives were in and cleaned up a config object that was just wasting time. BOP-403 ,BOP-434

--- packages/custom/lesson/public/controllers/lesson.js
+++ packages/custom/lesson/public/controllers/lesson.js
@@ -63,23 +63,9 @@
       }
     };
 
-    $scope.subjectAreaOptions = [
-            'Art',
-            'Agriscience & Technology',
-            'Arithmetic',
-            'Algebra',
-            'Band',
-            'Biology',
-            'Chemistry',
-            'Civics'
-          ];
-          
-    $scope.vocabOptions = [
-        'Art','Ecosystem','Hypothesis', 'Oyster', 'Science'
-    ];
-
-    //TODO - make this a service
-    $scope.nycScienceScopeOptions = [
+    //TODO - make these a service
+    $scope.defaultArrays = {
+      nycScienceScopeOptions: [
         "PS1A Structure and Properties of matter",
         "PS1B Chemical Reactions",
         "PS1C Nuclear Processes",
@@ -91,186 +77,27 @@
         "ESS2C The Roles of Water in Earth’s Surface Processes",
         "PS1 Matter and Its Interactions",
         "Grade 8, Unit1: Humans and the Environment"
-    ];
-    $scope.lessonConfig = [{
-      'blockTitle': 'Lesson Upload',
-      'blockItems': {
-        'title': {
-          'label': 'Lesson Title',
-          'required': true,
-          'colClass': 'col-md-8',
-          'type': 'text'
-        },
-        'unit': {
-          'label': 'unit',
-          'required': true,
-          'colClass': 'col-md-4',
-          'type': 'select',
-          'options': [
-            'Action', 'Field'
-          ]
-        }
-      }
-    }, {
-      'blockTitle': 'Lesson Overview',
-      'blockItems': {
-        'grade': {
-          'label': 'Grade',
-          'required': true,
-          'colClass': 'col-md-8',
-          'type': 'select',
-          'options': [
-            '6-8', '4th', '5th', '6th', '7th', '8th'
-          ]
-        },
-        'classPeriod': {
-          'label': 'Class Period',
-          'required': true,
-          'colClass': 'col-md-4',
-          'type': 'select',
-          'options': [
-            '1', '2', '3', '4', '5', '6', '7', '8'
-          ]
-        },
-        'setting': {
-          'label': 'Setting',
-          'required': true,
-          'colClass': 'col-md-4',
-          'type': 'select',
-          'options': [
-            'Classroom', 'Field'
-          ]
-        },
-        'subjectAreas': {
-          'label': 'Subject Area(s)',
-          'required': true,
-          'colClass': 'col-md-4',
-          'type': 'select',
-          'options': [
-            'Art',
-            'Agriscience & Technology',
-            'Arithmetic',
-            'Algebra',
-            'Band',
-            'Biology',
-            'Chemistry',
-            'Civics'
-          ]
-        },
-        'protocolConnections': {
-          'label': 'Protocol Connections',
-          'required': true,
-          'colClass': 'col-md-4',
-          'type': 'text'
-        },
-        'lessonSummary': {
-          'label': 'Lesson Summary',
-          'required': true,
-          'colClass': 'col-md-4',
-          'type': 'text'
-        }
-      }
-    }, {
-      'blockTitle': 'Lesson Objectives',
-      'blockItems': {
-        'objectives': {
-          'label': 'Objectives',
-          'required': true,
-          'type': 'text'
-        }
-      }
-    }, {
-      'blockTitle': 'Materials & Resources Objectives',
-      'blockItems': {
-        'supplies': {
-          'label': 'Supplies',
-          'required': true,
-          'type': 'text'
-        },
-        'teacherResources': {
-          'label': 'Teacher Resources',
-          'required': true,
-          'type': 'text'
-        },
-        'handouts': {
-          'label': 'Objectives',
-          'required': false,
-          'type': 'text'
-        },
-        'vocabulary': {
-          'label': 'Vocabulary',
-          'required': false,
-          'type': 'text'
-        }
-      }
-    }, {
-      'blockTitle': 'Background',
-      'blockItems': {
-        'background': {
-          'label': 'Background',
-          'required': false,
-          'type': 'text'
-        }
-      }
-    }, {
-      'blockTitle': 'Instruction Plan',
-      'partialRequire': 1,
-      'blockItems': {
-        'engage': {
-          'label': 'Engage',
-          'required': false,
-          'type': 'text'
-        },
-        'explore': {
-          'label': 'Explore',
-          'required': false,
-          'type': 'text'
-        },
-        'explain': {
-          'label': 'Explain',
-          'required': false,
-          'type': 'text'
-        },
-        'elaborate': {
-          'label': 'Elaborate',
-          'required': false,
-          'type': 'text'
-        },
-        'evaluate': {
-          'label': 'Evaluate',
-          'required': false,
-          'type': 'text'
-        }
-      }
-    }, {
-      'blockTitle': 'Standards',
-      'partialRequire': 1, //TODO - Logic for error message if at least one of the blockItems is not set
-      'blockItems': {
-        'nycScienceScopeSequence': {
-          'label': 'NEW YORK CITY SCIENCE SCOPE & SEQUENCE',
-          'required': false,
-          'type': 'text'
-        },
-        'ngssStandards': {
-          'label': 'NGSS STANDARDS',
-          'required': false,
-          'type': 'text'
-        },
-        'commonCoreEla': {
-          'label': 'COMMON CORE ELA',
-          'required': false,
-          'type': 'text'
-        },
-        'commonCoreMath': {
-          'label': 'COMMON CORE MATH',
-          'required': false,
-          'type': 'text'
-        }
-      }
-    }];
+      ],
+      vocabOptions: [
+        'Art', 'Ecosystem', 'Hypothesis', 'Oyster', 'Science'
+      ],
+      subjectAreaOptions: [
+        'Art',
+        'Agriscience & Technology',
+        'Arithmetic',
+        'Algebra',
+        'Band',
+        'Biology',
+        'Chemistry',
+        'Civics'
+      ],
+      unit: ['Action', 'Field'],
+      grade: ['6-8', '4th', '5th', '6th', '7th', '8th'],
+      classPeriod: ['1', '2', '3', '4', '5', '6', '7', '8'],
+      setting: ['Classroom', 'Field']
+    };
+
     $scope.onSubmit = function() {
-      console.log('here')
-        // alert(JSON.stringify(vm.model), null, 2);
       Lesson.addLesson($scope.sendModel);
     }
   }
